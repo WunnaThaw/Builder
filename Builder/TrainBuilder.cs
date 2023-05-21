8943589496b514c65cde284bf77713e1d4052b53
WunnaThaw <wunna.pe@gmail.com>
2023-05-21 20:41:41 +0630

    Builder Design Pattern

---
+++ Builder/TrainBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    /// <summary>
+    /// The 'ConcreteBuilder3' class
+    /// </summary>
+
+    class TrainBuilder : VehicleBuilder
+    {
+        public TrainBuilder()
+        {
+            vehicle = new Vehicle("Train");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Train Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "0";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "20";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "10";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleBuilder builder;
+
+            // Create shop with vehicle builders
+            Store store = new Store();
+
+            // Construct and display vehicles
+            builder = new TrainBuilder();
+            store.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            store.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            store.Construct(builder);
+            builder.Vehicle.Show();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
